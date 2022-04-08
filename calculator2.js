036f9512e0fdb9fb46a72dedd8e47d225c269ff8
Duvan Montoya <65972786+Duvan-Alejandro-Montoya-Rivas@users.noreply.github.com>
2022-04-07 21:20:35 -0500

    Create of function pow

--- calculator2.js
+++ calculator2.js
@@ -11,6 +11,11 @@
 //Divide
 
 
+//pow
+function pow(a,b){
+  showResult('su resultado base: '+ a + ', exponente: '+ b +':', Math.pow(a,b))
+}   
+
 //Helper functions
 function showResult(operation, result) {
     console.log(operation + ': ' + result)
