d25139e34c6fb854547e2db1f63b4f7c7eb5cf07
Hrojas26 <103230076+Hrojas26@users.noreply.github.com>
2022-04-07 21:20:34 -0500

    crear !

--- calculator2.js
+++ calculator2.js
@@ -10,6 +10,11 @@
 
 //Divide
 
+// abs
+function absoluto(a) { 
+    showResult('el valor absoluto de  ('+a+')' +'es',Math.abs(a));
+
+  }
 
 //Helper functions
 function showResult(operation, result) {
